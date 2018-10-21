81f5ea5bbd9e12d1cd498a578aa6dae758053fc9
nour20-meet <nour20@meet.mit.edu>
2018-10-21 10:58:34 -0400

    first commit

---
+++ lab1.py
@@ -0,0 +1,11 @@
+print("nour"*100)
+number1=3
+print(number1)
+number2=number1/2
+print(number2)
+list1=[2,3,6]
+total=0
+
+for elmint in list1:
+	total=total+elmint
+print(total)
---
+++ lab2.py
@@ -0,0 +1,14 @@
+y=[1,2,4,6,8]
+def thisthing(y):
+	x=[y[0],y[-1]]
+	return x
+thisthing(y)
+print(thisthings(y))
+
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+if index in a =<6:
+for index in a :
+	if a =< 6:
+		print(index)
