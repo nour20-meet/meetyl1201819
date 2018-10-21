d8a01464060f44ea1721158e9ace153be24a6959
nour20-meet <nour20@meet.mit.edu>
2018-10-21 12:26:34 -0400

    what ever

--- lab2.py
+++ lab2.py
@@ -1,14 +1,15 @@
 y=[1,2,4,6,8]
+x=[]
 def thisthing(y):
 	x=[y[0],y[-1]]
 	return x
-thisthing(y)
-print(thisthings(y))
+print(thisthing(y))
 
 
-
+'''
 a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-if index in a =<6:
-for index in a :
-	if a =< 6:
-		print(index)
+if index in a <6:
+	for index in a :
+		if a =< 6:
+print(index)
+'''
