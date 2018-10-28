696de709271346aa0d2f188779f37d928904d3a8
nour20-meet <nour20@meet.mit.edu>
2018-10-28 13:18:16 -0400

    lab4

---
+++ lab4.py
@@ -0,0 +1,12 @@
+class Aminal(object):
+	def __init__(self,name,favfood):
+		self.name = name 
+		self.favfood = favfood
+	def eat(self.food):
+		print (self.name + 'is eating' + food)
+	def description (self):
+		print(self.name + 'is'+ age + 'years old' + 'is eating' + self.favfood)
+
+s = Animal		
+	
+	
