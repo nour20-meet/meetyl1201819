6621132aba560aad25c2e4b4932cc444ce78bff1
nour20-meet <nour20@meet.mit.edu>
2018-12-16 10:30:11 -0500

    update all

--- lab6.py
+++ lab6.py
@@ -1,34 +1,46 @@
 import turtle
 from turtle import Turtle
 import random 
+
+# 1
 turtle.colormode(255)
 
 class square(Turtle):
-	def __init__(self, size):
+	def __init__(self, size ,color):
 		Turtle.__init__(self)
-		self.shape("square")
 		self.shapesize(size)
-	def rand_color(self):
+		self.color(color)
+		self.shape("square")
+	def random_color(self):
 		r=random.randint(0,256)
 		b=random.randint(0,256)
 		g=random.randint(0,256)
 		self.color(r,g,b)
 
-x=square(9)
-x.rand_color()	
+square1 = square(5, "red")
+square1.random_color()
 
-turtle.mainloop()	
-
-class hexagon(Turtle):
+# 2
 
 turtle.home()
+turtle.pu()
 turtle.begin_poly()
-turtle.fd(100)
-turtle.left(20)
-turtle.fd(30)
-turtle.left(60)
-turtle.fd(50)
-turtle.end_poly()
-p = turtle.get_poly()
-turtle.register_shape("hexagon",hexagon)
-turtle.mainloop()
+for i in range (6):
+	turtle.fd(100)
+	turtle.right(60)
+
+turtle.end_poly()	
+hexagon = turtle.get_poly()
+turtle.register_shape("hexagon", hexagon)
+
+
+class hexagon(turtle):
+	def__init__(self,size,speed,color):
+       Turtle.__init__(self)
+       self.shapesize(size)
+       self.shape("hexagon")
+       self.color(color)
+       self.speed(speed)
+
+p = hexagon(2,0,"blue")  
+turtle.mainloop()     
