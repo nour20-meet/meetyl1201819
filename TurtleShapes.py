04bd90ba589550c1866ca2e8ca3942ea801e55d8
nour20-meet <nour20@meet.mit.edu>
2019-01-22 09:50:34 -0500

    commit

---
+++ TurtleShapes.py
@@ -0,0 +1,24 @@
+import turtle
+# x=200
+# y=0
+# for i in range(5):
+# 	turtle.pencolor("pink")
+# 	turtle.forward(100)
+# 	turtle.right (145)
+
+# turtle.pencolor("light blue")
+# turtle.goto(50,-150)
+# turtle.pencolor("light green")
+# turtle.goto(100,100)
+# turtle.pencolor("purple")
+# turtle.goto(150,-150)
+# turtle.pencolor("blue")
+#turtle.goto(0,0)
+turtle.fillcolor("light blue")
+turtle.begin_fill()
+turtle.goto(100,0)
+turtle.goto(100,-100)
+turtle.goto(50,-200)
+turtle.goto(0,-100)
+turtle.goto(0,0)
+turtle.end_fill()
